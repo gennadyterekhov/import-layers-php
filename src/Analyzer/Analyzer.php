36b908eb8545c56d5eb8b2de3a20bf49e9d18cfe
Gennady Terekhov <ter.ge@yandex.ru>
2025-10-06 18:03:23 +0300

    analyzer

--- src/Analyzer/Analyzer.php
+++ src/Analyzer/Analyzer.php
@@ -4,40 +4,25 @@ declare(strict_types=1);
 
 namespace Gennadyterekhov\ImportLayersPhp\Analyzer;
 
-use Composer\Autoload\ClassLoader;
 use Gennadyterekhov\ImportLayersPhp\Dto\AnalysisResult;
 use Gennadyterekhov\ImportLayersPhp\Dto\Config;
-use PhpParser\NodeTraverser;
-use PhpParser\NodeVisitor\NameResolver;
-use PhpParser\Parser;
-use PhpParser\ParserFactory;
-use PhpParser\PrettyPrinter\Standard;
-use PHPUnit\TextUI\Configuration\File;
+use Gennadyterekhov\ImportLayersPhp\Project\Project;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
-use ReflectionClass;
 use RegexIterator;
 use Throwable;
 
 final readonly class Analyzer
 {
     public function __construct(
-        private NodeTraverser $traverser,
         private FileAnalyzer $fileAnalyzer,
     ) {}
 
     public function analyze(Config $config): AnalysisResult
     {
         $errors = [];
-        // TODO config dirs here
-        $inDir = $this->getProjectRoot() . '/src';
-        $outDir = '/some/other/path';
+        $inDir = Project::getProjectRoot() . '/src';
 
-        // TODO config visitors here
-        $this->traverser->addVisitor(new NameResolver); // we will need resolved names
-        // $traverser->addVisitor(new NamespaceConverter); // our own node visitor
-
-        // iterate over all .php files in the directory
         $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($inDir));
         $files = new RegexIterator($files, '/\.php$/');
 
@@ -50,15 +35,6 @@ final readonly class Analyzer
             }
         }
 
-        $analysis = new AnalysisResult($errors);
-        return $analysis;
-    }
-
-    private function getProjectRoot(): string
-    {
-        $reflection = new ReflectionClass(ClassLoader::class);
-        $vendorDir = dirname($reflection->getFileName(), 2);
-
-        return dirname($vendorDir);
+        return new AnalysisResult($errors);
     }
 }
