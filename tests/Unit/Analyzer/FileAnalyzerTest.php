3060f29dbd17269e274cc91b3199391a7b3b1da9
Gennady Terekhov <ter.ge@yandex.ru>
2025-10-06 17:06:08 +0300

    analyzer

---
+++ tests/Unit/Analyzer/FileAnalyzerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Analyzer;
+
+use Gennadyterekhov\ImportLayersPhp\Analyzer\Analyzer;
+use Gennadyterekhov\ImportLayersPhp\Analyzer\FileAnalyzer;
+use Gennadyterekhov\ImportLayersPhp\Dto\Config;
+use PHPUnit\Framework\TestCase;
+use PhpParser\ParserFactory;
+use PhpParser\NodeTraverser;
+use SplFileInfo;
+use Tests\Helpers\Testdata;
+
+final class FileAnalyzerTest extends TestCase
+{
+    public function testCanAnalyzeFile(): void
+    {
+        $parser = (new ParserFactory())->createForHostVersion();
+        $traverser = new NodeTraverser;
+        $config = new Config();
+        $fa = new FileAnalyzer($parser, $traverser);
+        [$path, $onlyFuncName] = Testdata::getPathAndFuncName(__METHOD__);
+
+        $fileInfo = new SplFileInfo($path);
+        $res = $fa->analyzeFile($config, $fileInfo);
+        self::assertCount(0, $res->errors);
+    }
+}
