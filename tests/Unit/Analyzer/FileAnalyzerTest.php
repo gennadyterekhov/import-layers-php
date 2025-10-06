a2482de96a84c2b5236f22cb475b9818528d9209
Gennady Terekhov <ter.ge@yandex.ru>
2025-10-06 17:12:12 +0300

    analyzer

--- tests/Unit/Analyzer/FileAnalyzerTest.php
+++ tests/Unit/Analyzer/FileAnalyzerTest.php
@@ -23,8 +23,26 @@ final class FileAnalyzerTest extends TestCase
         $fa = new FileAnalyzer($parser, $traverser);
         [$path, $onlyFuncName] = Testdata::getPathAndFuncName(__METHOD__);
 
+        var_dump($path);
+        $path = $path . 'LowUsesHigh/Low.php';
         $fileInfo = new SplFileInfo($path);
         $res = $fa->analyzeFile($config, $fileInfo);
         self::assertCount(0, $res->errors);
+    }
+
+    public function testHighCannotUseLow(): void
+    {
+        $parser = (new ParserFactory())->createForHostVersion();
+        $traverser = new NodeTraverser;
+        $config = new Config();
+        $fa = new FileAnalyzer($parser, $traverser);
+        [$path, $onlyFuncName] = Testdata::getPathAndFuncName(__METHOD__);
+
+        var_dump($path);
+        $path = $path . 'HighUsesLow/High.php';
+        $fileInfo = new SplFileInfo($path);
+        $res = $fa->analyzeFile($config, $fileInfo);
+        self::assertCount(1, $res->errors);
+        self::assertEquals('Cannot use Low from High', $res->errors[0]);
     }
 }
