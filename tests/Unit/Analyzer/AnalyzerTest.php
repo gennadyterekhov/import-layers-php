31992f67c7e79ce8b91a3fac8cfbb3c421ceb559
Gennady Terekhov <ter.ge@yandex.ru>
2025-10-06 16:47:30 +0300

    analyzer

--- tests/Unit/Analyzer/AnalyzerTest.php
+++ tests/Unit/Analyzer/AnalyzerTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tests\Unit\Analyzer;
 
 use Gennadyterekhov\ImportLayersPhp\Analyzer\Analyzer;
-use Gennadyterekhov\ImportLayersPhp\Config\ConfigService;
 use Gennadyterekhov\ImportLayersPhp\Dto\Config;
 use PHPUnit\Framework\TestCase;
 
@@ -15,9 +14,7 @@ final class AnalyzerTest extends TestCase
     {
         $config = new Config();
         $service = new Analyzer();
-        $config = $service->analyze($config);
-        self::assertEquals(false, $config->debug);
-        self::assertEquals(false, $config->ignoreTests);
-        self::assertEquals('src/Dto', $config->layers[0]);
+        $res = $service->analyze($config);
+        self::assertCount(0, $res->errors);
     }
 }
