ea22a34724c02428c305e0d56c26a7906777a8f1
Gennady Terekhov <ter.ge@yandex.ru>
2025-10-06 16:29:57 +0300

    config

--- src/Config/ConfigService.php
+++ src/Config/ConfigService.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Gennadyterekhov\ImportLayersPhp\Config;
 
+use Composer\Autoload\ClassLoader;
 use Gennadyterekhov\ImportLayersPhp\Dto\Config;
+use ReflectionClass;
 
 final readonly class ConfigService
 {
@@ -12,6 +14,30 @@ final readonly class ConfigService
 
     public function readConfigFromFileIntoDto(): Config
     {
-        return new Config();
+        $data = json_decode($this->getConfigFileContents(), true);
+
+        return new Config(
+            $data['debug'] ?? false,
+            $data['ignoreTests'] ?? false,
+            $data['layers'] ?? [],
+        );
+    }
+
+    private function getConfigFileContents(): string
+    {
+        $path = $this->getProjectRoot() . '/import_layers.json';
+        $contents = file_get_contents($path);
+        if ($contents === false) {
+            return '';
+        }
+        return $contents;
+    }
+
+    private function getProjectRoot(): string
+    {
+        $reflection = new ReflectionClass(ClassLoader::class);
+        $vendorDir = dirname($reflection->getFileName(), 2);
+
+        return dirname($vendorDir);
     }
 }
