60488d31344781577015f987d87050f5d50f2ec7
Gennady Terekhov <ter.ge@yandex.ru>
2025-10-06 18:01:50 +0300

    analyzer

--- src/Analyzer/FileAnalyzer.php
+++ src/Analyzer/FileAnalyzer.php
@@ -4,50 +4,73 @@ declare(strict_types=1);
 
 namespace Gennadyterekhov\ImportLayersPhp\Analyzer;
 
-use Composer\Autoload\ClassLoader;
+use Exception;
 use Gennadyterekhov\ImportLayersPhp\Dto\AnalysisResult;
 use Gennadyterekhov\ImportLayersPhp\Dto\Config;
-use PhpParser\NodeTraverser;
-use PhpParser\NodeVisitor\NameResolver;
 use PhpParser\Parser;
-use PhpParser\ParserFactory;
-use PhpParser\PrettyPrinter\Standard;
-use ReflectionClass;
 use SplFileInfo;
 use Throwable;
+use PhpParser\Node\Stmt\Namespace_;
+use PhpParser\Node\Stmt\Use_;
 
 final readonly class FileAnalyzer
 {
     public function __construct(
         private Parser $parser,
-        private NodeTraverser $traverser,
     ) {}
 
     public function analyzeFile(Config $config, SplFileInfo $fileInfo): AnalysisResult
     {
         $errors = [];
-
         try {
-            var_dump($fileInfo->getPathName());
-
-            // read the file that should be converted
             $code = file_get_contents($fileInfo->getPathName());
 
-            // parse
             $stmts = $this->parser->parse($code);
+            if ($stmts === null) {
+                throw new Exception('could not parse file ' . $fileInfo->getPathname());
+            }
 
-            var_dump($stmts);
-
-            // traverse
-            $stmts = $this->traverser->traverse($stmts);
-
-//                var_dump($stmts);
-
+            foreach ($stmts as $stmt) {
+                if ($stmt::class === Namespace_::class) {
+                    foreach ($stmt->stmts as $item) {
+                        if ($item::class === Use_::class) {
+                            $this->analyzeUse($config, $stmt->name->name, $item);
+                        }
+                    }
+                }
+            }
         } catch (Throwable $exception) {
             echo 'Parse Error: ', $exception->getMessage();
             $errors[] = $exception->getMessage();
         }
-        $analysis = new AnalysisResult($errors);
-        return $analysis;
+
+        return new AnalysisResult($errors);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function analyzeUse(Config $config, string $namespace, Use_ $useClause): void
+    {
+        foreach ($useClause->uses as $useItem) {
+            $currentLayer = $this->getLayer($config, $namespace);
+            $layerThatYoureTryingToImport = $this->getLayer($config, $useItem->name->name);
+            if ($currentLayer > $layerThatYoureTryingToImport) {
+                throw new Exception('Cannot import ' . $useItem->name->name . ' from ' . $namespace);
+            }
+        }
+    }
+
+    private function getLayer(Config $config, string $namespace): int
+    {
+        $total = count($config->layers);
+        for ($i = 0; $i < $total; $i++) {
+            $layerName = $config->layers[$i];
+            if (str_contains($namespace, $layerName)) {
+                return $total - $i;
+            }
+        }
+
+        return 0;
     }
 }
