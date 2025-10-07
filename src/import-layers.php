05a711b0c0870e3472ca0066e3408e36481c4293
Gennady Terekhov <ter.ge@yandex.ru>
2025-10-07 09:54:05 +0300

    analyzer

--- src/import-layers.php
+++ src/import-layers.php
@@ -1,6 +1,18 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+// Try Composer's global path first
+if (isset($GLOBALS['_composer_autoload_path'])) {
+    require_once $GLOBALS['_composer_autoload_path'];
+} // Fallback to relative path (for development)
+else if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+} // Fallback to typical vendor location when installed as dependency
+else if (file_exists(__DIR__ . '/../../autoload.php')) {
+    require_once __DIR__ . '/../../autoload.php';
+} else {
+    fwrite(STDERR, "Could not find autoloader\n");
+    exit(1);
+}
 
 use Gennadyterekhov\ImportLayersPhp\Analyzer\Analyzer;
 use Gennadyterekhov\ImportLayersPhp\Config\ConfigService;
